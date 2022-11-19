8ee0afcc93dcf8e95f67696dc4adbdb04455749d
Jessica Kline <kline.jessica@gmail.com>
2022-11-19 14:04:33 -0600

    Navigation start

--- src/app/wall/wall.component.ts
+++ src/app/wall/wall.component.ts
@@ -13,6 +13,20 @@ export class WallComponent implements OnInit {
   photos: any;
   displayCustom: boolean;
   activeIndex: number = 0;
+  responsiveOptions:any[] = [
+    {
+        breakpoint: '1024px',
+        numVisible: 5
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1
+    }
+];
 
   constructor(private photoService: PhotoService) { 
     this.displayCustom = false;
